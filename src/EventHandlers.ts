434f8de2227b8873c893b2e38e385defdd85d7ca
denhampreen <denhampreen@gmail.com>
2024-06-12 10:16:24 +0100

    chore: update contract and abi

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -1,3 +1,4 @@
+// https://github.com/poolshark-protocol/limit-sway/blob/abi-generation/contracts/pool/src/limit_pool.sw
 import { LimitPoolContract } from "generated";
 
 LimitPoolContract.InitPoolEvent.loader(({ event, context }) => {});
@@ -10,6 +11,27 @@ LimitPoolContract.InitPoolEvent.handler(({ event, context }) => {
 LimitPoolContract.SwapEvent.loader(({ event, context }) => {});
 
 LimitPoolContract.SwapEvent.handler(({ event, context }) => {
+  console.log("event.data");
+  console.log(event.data);
+});
+
+LimitPoolContract.MintLimitEvent.loader(({ event, context }) => {});
+
+LimitPoolContract.MintLimitEvent.handler(({ event, context }) => {
+  console.log("event.data");
+  console.log(event.data);
+});
+
+LimitPoolContract.BurnLimitEvent.loader(({ event, context }) => {});
+
+LimitPoolContract.BurnLimitEvent.handler(({ event, context }) => {
+  console.log("event.data");
+  console.log(event.data);
+});
+
+LimitPoolContract.BurnRangeEvent.loader(({ event, context }) => {});
+
+LimitPoolContract.BurnRangeEvent.handler(({ event, context }) => {
   console.log("event.data");
   console.log(event.data);
 });
